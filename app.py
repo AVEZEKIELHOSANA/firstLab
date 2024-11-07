d0cb5046db349fd6d0b37ea41e7141cf70a2f3f6
AVEZEKIELHOSANA <hosanaezekiel21@gmail.com>
2024-11-07 15:28:56 +0100

    TESTING

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
- 
+  
+from flask import Flask, jsonify
+
+app = Flask(_name_)
+
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello level 400 FET, Quality Assurance!")
+
+
+if _name_ == '_main_':
+    app.run(debug=True)
